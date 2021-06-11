f26f2ad9ceb5e12202e1df40bc8b0a24673761c2
Weston Ellis <ellisweston112@gmail.com>
2021-06-11 16:21:08 -0600

    Add developer model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+ruby_developer = {
+  name: "Billy Bob Burton",
+  passion: "fruit",
+  number_of_monitors: 26,
+  number_of_braincells: 13.6,
+  sane: false,
+  caffinated: true,
+  projects: ["Hunting Python for Dummies", "Cthulu and You: how to glean sanity one eldritch abomination at a time!", "Avoid Caffine Overdose in One Easy Step!"],
+  known_languages: ["English", "Highschool Spanish"]
+}
