5cbe655fa57589a464125c658f37af0b6bcb96e8
Weston Ellis <ellisweston112@gmail.com>
2021-06-11 16:01:23 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,13 @@
+movie = "The Princess Bride"
+is_entertaining = true
+movie = "Star Wars: a New Hope"
+
+num_4 = 4
+num_6 = 6
+
+product = num_4 * num_6
+
+favorite_foods = ["Hashbrowns", "Pizza", "Pasta"]
+favorite_foods.shift
+
+movie.size
