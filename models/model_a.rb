74cf2ce0069380ae871298c90c80fc0a6cac4e47
Weston Ellis <ellisweston112@gmail.com>
2021-06-11 16:13:03 -0600

    Add dinosaur model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+t_rex = {
+  "favorite food" => "Hadrosaur",
+  "name" => "Sue",
+  "number of teeth" => 6,
+  "kill count" => 1800,
+  "is alive" => false,
+  "is complete" => false,
+  "hands" => ["left hand", "right hand"],
+  "children" => ["spike", "spikette", "billy", "ol' what's his face"]
+}
